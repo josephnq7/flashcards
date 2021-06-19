25970351ef1550dd3f1b08425499b3d91c431c13
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-06-19 12:00:20 -0700

    example of loop in rendering

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+  ];
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -10,7 +19,8 @@ app.get('/', (req, res) => {
 app.get('/cards', (req, res) => {
     res.render('card', {
         prompt: "Who is buried in Grant's tomb?",
-        hint: "Think about whose tomb it is."
+        hint: "Think about whose tomb it is.",
+        colors,
     });
 });
 
