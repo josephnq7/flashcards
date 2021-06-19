706bea2eaf83ac315e2692eb84dc7d9c3ad7f2e5
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-06-19 11:49:45 -0700

    renderig view in Express

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('<h1>I Love Vi<h1>');
+    res.render('index')
 });
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello Javascript Developer!<h1>');
+app.get('/cards', (req, res) => {
+    res.render('card', {
+        prompt: "Who is buried in Grant's tomb?",
+        hint: "Think about whose tomb it is."
+    });
 });
 
 app.listen(3000, () => {
