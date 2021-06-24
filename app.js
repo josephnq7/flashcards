9c5dcbf23125203f44cf6d95bdfa6c764559da41
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-06-23 21:46:09 -0700

    POST request

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
     res.render('index')
 });
 
+app.get('/hello', (req, res) => {
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+    res.render('hello');
+});
+
 app.get('/cards', (req, res) => {
     res.render('card', {
         prompt: "Who is buried in Grant's tomb?",
