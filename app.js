8b98c2ed10b643dbb5044a8c64f3ade0c113d771
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-07-11 17:21:27 -0700

    Middleware with error handler

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ const colors = [
 
 app.set('view engine', 'pug');
 
+app.use((req, res, next) => {
+    console.log("Hello");
+    // const err = new Error("Oh noes!");
+    // err.status = 500;
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("world!");
+    next();
+});
+
 app.get('/', (req, res) => {
     const name = req.cookies.username;
     if (name) {
@@ -55,6 +67,18 @@ app.get('/cards', (req, res) => {
         hint: "Think about whose tomb it is.",
         colors,
     });
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found!');
+    err.status = 404;    
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
