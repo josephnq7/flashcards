49778ec23aa5abc3868a37343247c37563dbac85
Hiep Nguyen <hiepnguyen@Hieps-MacBook-Pro.local>
2021-07-18 18:57:43 -0700

    dynamically loading card by random

--- routes/cards.js
+++ routes/cards.js
@@ -4,15 +4,26 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+    const flashCardId = Math.floor(Math.random() * numberOfCards);
+    res.redirect(`/cards/${flashCardId}`);
+});
+
 
 router.get('/:id', (req, res) => {
     const {side} = req.query;
     const {id} = req.params;
 
+    if (!side) {
+        res.redirect(`/cards/${id}?side=question`);
+    }
+
+    const name = req.cookies.username;
     const text = cards[id][side];
     const {hint} = cards[id];
 
-    const templateData = {id, text};
+    const templateData = {id, text, name};
 
     if (side === 'question') {
         templateData.hint = hint;
